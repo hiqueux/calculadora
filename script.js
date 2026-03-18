01fb8b655d21b6fd6975e32b7181d639288c7028
hiqueux <henrique_tonioti@estudante.sesisenai.org.br>
2026-03-18 09:02:08 -0300

    feat - adicionei função para verificar se ja foi clicado na "," no visor e verificar botão se clicou em números

--- script.js
+++ script.js
@@ -10,3 +10,24 @@ function updateResultado(originClear = false) {
     resultado.innerText = originClear ? 0 : currentNumber.replace(".", ",");
 }
 
+function addDigit(digit){
+    if (digit === "," && (currentNumber.includes(",") || !currentNumber)) return;
+
+    if (restart) {
+        currentNumber = digit;
+        restart = false;
+    } else {
+        currentNumber += digit;
+    }
+
+    updateResultado();
+}
+
+botão.forEach((button) => {
+    buttom.addEventListener("click", () => {
+        const buttonText = button.innerText;
+        if (/^[0-9,]+$/.test(buttonText)) {
+            addDigit(buttonText);
+        }
+    });
+});
